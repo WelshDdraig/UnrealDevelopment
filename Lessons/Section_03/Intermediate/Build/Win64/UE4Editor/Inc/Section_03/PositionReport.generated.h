f88a4db1bcc5b712c1acb9656347adc168686395
WelshDdraig <alexparker@p10productions.net>
2018-01-24 21:46:09 +0000

    Position Reporter
    Generated code to begin editing for a positional reporter script on one of the chairs in the scene.

---
+++ Lessons/Section_03/Intermediate/Build/Win64/UE4Editor/Inc/Section_03/PositionReport.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SECTION_03_PositionReport_generated_h
+#error "PositionReport.generated.h already included, missing '#pragma once' in PositionReport.h"
+#endif
+#define SECTION_03_PositionReport_generated_h
+
+#define Section_03_Source_Section_03_PositionReport_h_13_RPC_WRAPPERS
+#define Section_03_Source_Section_03_PositionReport_h_13_RPC_WRAPPERS_NO_PURE_DECLS
+#define Section_03_Source_Section_03_PositionReport_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUPositionReport(); \
+	friend SECTION_03_API class UClass* Z_Construct_UClass_UPositionReport(); \
+public: \
+	DECLARE_CLASS(UPositionReport, UActorComponent, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/Section_03"), NO_API) \
+	DECLARE_SERIALIZER(UPositionReport) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Section_03_Source_Section_03_PositionReport_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesUPositionReport(); \
+	friend SECTION_03_API class UClass* Z_Construct_UClass_UPositionReport(); \
+public: \
+	DECLARE_CLASS(UPositionReport, UActorComponent, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/Section_03"), NO_API) \
+	DECLARE_SERIALIZER(UPositionReport) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Section_03_Source_Section_03_PositionReport_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UPositionReport(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UPositionReport) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPositionReport); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPositionReport); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UPositionReport(UPositionReport&&); \
+	NO_API UPositionReport(const UPositionReport&); \
+public:
+
+
+#define Section_03_Source_Section_03_PositionReport_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UPositionReport(UPositionReport&&); \
+	NO_API UPositionReport(const UPositionReport&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPositionReport); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPositionReport); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UPositionReport)
+
+
+#define Section_03_Source_Section_03_PositionReport_h_13_PRIVATE_PROPERTY_OFFSET
+#define Section_03_Source_Section_03_PositionReport_h_10_PROLOG
+#define Section_03_Source_Section_03_PositionReport_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Section_03_Source_Section_03_PositionReport_h_13_PRIVATE_PROPERTY_OFFSET \
+	Section_03_Source_Section_03_PositionReport_h_13_RPC_WRAPPERS \
+	Section_03_Source_Section_03_PositionReport_h_13_INCLASS \
+	Section_03_Source_Section_03_PositionReport_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Section_03_Source_Section_03_PositionReport_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Section_03_Source_Section_03_PositionReport_h_13_PRIVATE_PROPERTY_OFFSET \
+	Section_03_Source_Section_03_PositionReport_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	Section_03_Source_Section_03_PositionReport_h_13_INCLASS_NO_PURE_DECLS \
+	Section_03_Source_Section_03_PositionReport_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Section_03_Source_Section_03_PositionReport_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
